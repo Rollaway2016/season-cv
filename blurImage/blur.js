09d33d3a29612bb7ac57dc85c4e6913d34afd71f
Rollaway2016 <253640268@qq.com>
2016-10-25 14:14:27 +0800

    1.2.3
    Add 12 cards to blurImage.

--- blurImage/blur.js
+++ blurImage/blur.js
@@ -4,23 +4,26 @@ canvas.width = 300;
 canvas.height = 400;
 var context = canvas.getContext('2d');
 var image = new Image();
+//预设初始图片
 image.src = '../Gallery/image/妖道.jpg';
+context.drawImage(image, 0, 0);
+var card = document.getElementById('card');
 var radius = 50;
-var circle = {x: 0, y: 0, r: radius};
+var circle = {x: 0, y: 0, r: 500};
 // canvas图片载入完成后绘图到屏幕
 image.onload = function (e) {
-	randomCircle();
 	draw(image, circle);
 };
 function randomCircle() {
 	circle = {
-		x: randomInt(radius / 2, canvas.width - radius / 2),
-		y: randomInt(radius / 2, canvas.height - radius / 2),
+		x: randomInt(radius + 10, canvas.width - radius - 10),
+		y: randomInt(radius + 10, canvas.height - radius - 10),
 		r: radius
 	};
 }
 
 function draw(image, circle) {
+	image.src = card.getAttribute('src');
 	context.clearRect(0, 0, canvas.width, canvas.height);
 	context.save();
 	getClipArea(circle);
@@ -31,10 +34,18 @@ function draw(image, circle) {
 function getClipArea(circle) {
 	context.beginPath();
 	context.arc(circle.x, circle.y, circle.r, 0, Math.PI * 2, false);
+	context.strokeStyle = "#fff";
+	context.lineWidth = 10;
+	context.stroke();
 	context.clip();
+	context.closePath();
 }
 
 function reset() {
+	var randomCard = data[randomInt(0,data.length-1)];
+	var templateCard = '../Gallery/image/妖道.jpg';
+	var newCard = templateCard.replace('妖道', randomCard);
+	card.setAttribute('src', newCard);
 	randomCircle();
 	draw(image, circle);
 }
@@ -46,10 +57,25 @@ function show() {
 		if (circle.r >500) {
 			clearInterval(t);
 		}
-	}, 20)
+	}, 20);
 }
 
 function randomInt(min, max) {
 	var output = Math.floor(Math.random()*(max-min+1)+min);
 	return output;
 }
+
+var data = [
+'小青',
+'贝雷杰',
+'冰波利',
+'狂暴小恶魔',
+'米洛斯',
+'南瓜魂',
+'群叶猫',
+'人鱼士兵',
+'妖道',
+'直升机哥布林',
+'摇滚蝗虫',
+'狐仙'
+]
